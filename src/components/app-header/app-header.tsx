393ab0068b71e9ffb6e7f50245dfadc0334ebd21
Kirill Sheyanov <amocodingsystem@gmail.com>
2025-06-07 19:43:02 +0300

    links

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -1,4 +1,9 @@
 import { FC } from 'react';
+import { useSelector } from '../../services/store';
 import { AppHeaderUI } from '@ui';
 
-export const AppHeader: FC = () => <AppHeaderUI userName='' />;
+export const AppHeader: FC = () => {
+  // Заглушка для userName, так как Redux ещё не настроен
+  const userName = ''; // Будет браться из state.user позже
+  return <AppHeaderUI userName={userName} />;
+};
