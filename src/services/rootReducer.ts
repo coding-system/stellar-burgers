2d91bb5aab6023f8f98c122c048e8ec4e0dc8ee3
Kirill Sheyanov <amocodingsystem@gmail.com>
2025-06-12 15:08:36 +0300

    auth slice

--- src/services/rootReducer.ts
+++ src/services/rootReducer.ts
@@ -2,11 +2,13 @@ import { combineReducers } from '@reduxjs/toolkit';
 import ingredientsReducer from './slices/ingredientsSlice';
 import constructorReducer from './slices/constructorSlice';
 import feedReducer from './slices/feedSlice';
+import authReducer from './slices/authSlice';
 
 const rootReducer = combineReducers({
   ingredients: ingredientsReducer,
   burgerConstructor: constructorReducer,
-  feed: feedReducer
+  feed: feedReducer,
+  auth: authReducer
 });
 
 export default rootReducer;
