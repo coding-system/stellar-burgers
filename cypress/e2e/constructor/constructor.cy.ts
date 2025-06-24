2f782fcb8ea2c9b06513a84b8fb3c89d733015cc
Kirill Sheyanov <amocodingsystem@gmail.com>
2025-06-24 21:44:47 +0300

    O

--- cypress/e2e/constructor/constructor.cy.ts
+++ cypress/e2e/constructor/constructor.cy.ts
@@ -1,5 +1,28 @@
+/// <reference types="cypress" />
+
 describe('Страница конструктора', () => {
-  it('должен загружать страницу', () => {
-    cy.visit('');
+  beforeEach(() => {
+    cy.intercept('GET', '/api/ingredients', { fixture: 'ingredients.json' }).as(
+      'getIngredients'
+    );
+    cy.intercept('GET', '/api/auth/user', { fixture: 'user.json' }).as(
+      'getUser'
+    );
+    cy.intercept('POST', '/api/orders', { fixture: 'order.json' }).as(
+      'postOrder'
+    );
+    cy.visit('/');
+    cy.wait('@getIngredients');
+  });
+
+  it('отображает все ингредиенты из fixtures/ingredients.json', () => {
+    cy.fixture('ingredients.json').then((data) => {
+      (data.data as { name: string }[]).forEach((ingredient) => {
+        cy.get('[data-cy=ingredient-card]').should(
+          'contain.text',
+          ingredient.name
+        );
+      });
+    });
   });
 });
