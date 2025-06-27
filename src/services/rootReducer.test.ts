e81e006e72b2b09b68115403f7a1915a18f33942
Kirill Sheyanov <amocodingsystem@gmail.com>
2025-06-27 03:04:30 +0300

    fix 4

--- src/services/rootReducer.test.ts
+++ src/services/rootReducer.test.ts
@@ -8,15 +8,16 @@ import userOrdersSlice from './slices/userOrders/userOrdersSlice';
 
 describe('Тесты rootReducer', () => {
   it('должен правильно инициализироваться с начальным состоянием', () => {
-    const state = rootReducer(undefined, { type: 'unknown' });
+    const initAction = { type: '@@INIT' };
+    const state = rootReducer(undefined, initAction);
 
     expect(state).toEqual({
-      burgerConstructor: constructorSlice(undefined, { type: 'unknown' }),
-      ingredients: ingredientsSlice(undefined, { type: 'unknown' }),
-      feed: feedSlice(undefined, { type: 'unknown' }),
-      order: orderReducer(undefined, { type: 'unknown' }),
-      auth: authSlice(undefined, { type: 'unknown' }),
-      userOrders: userOrdersSlice(undefined, { type: 'unknown' })
+      burgerConstructor: constructorSlice(undefined, initAction),
+      ingredients: ingredientsSlice(undefined, initAction),
+      feed: feedSlice(undefined, initAction),
+      order: orderReducer(undefined, initAction),
+      auth: authSlice(undefined, initAction),
+      userOrders: userOrdersSlice(undefined, initAction)
     });
   });
 });
